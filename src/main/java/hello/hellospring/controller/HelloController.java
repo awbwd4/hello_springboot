1aa4558815f7926071c13b75d609d2f9d5525b47
awbwd4 <awbwd4@gmail.com>
2022-04-21 16:15:07 +0900

    22.-4.21 static page & mvc, temaplate engine

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,8 +12,19 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data1", "hello!!");
         model.addAttribute("data2", "helloddddd!!");
+        System.out.println("hello!!");
         return "hello";
         // 컨트롤러에서 리턴값으로 문자를 반환하면 view resolver가 templates 디렉토리 안의
         // hello.html을 알아서 찾아서 렌더링해준다.
     }
+
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(required = true, value = "name") String name, Model model){
+        model.addAttribute("name", name);
+        System.out.println("hello-mvc");
+        return "hello-template";
+    }
+
+
 }
