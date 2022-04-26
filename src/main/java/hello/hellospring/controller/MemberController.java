ff01ffdb5209e4a66c2e504ec0df084407641b6b
awbwd4 <awbwd4@gmail.com>
2022-04-26 21:10:25 +0900

    22.04.26 Member Enrollment and list

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -27,5 +33,28 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         //Controller 생성자를 생성할때 memberservice객체도 같이 생성한다
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        System.out.printf("form.getName : %s", form.getName());
+        member.setName(form.getName());
+
+        memberService.join(member);
+        System.out.println("============");
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
